0e15e81bfcc04972772f92430b11765b9bc64a8b
edmund <edmundtay96@gmail.com>
2019-01-04 09:42:13 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4f1fa15fb799e3ee9b08.js"
+    "url": "webpack-runtime-ea1543a86db18220e57c.js"
   },
   {
     "url": "app-265c76b86b4a445a92bd.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5039a525d4fcea4395bca6dc0439786e"
+    "revision": "03a67628893776b72872861677e81fc5"
   },
   {
     "url": "0.9cda76730c110765b5f9.css"
