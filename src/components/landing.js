2e785b37c1d901cc673d489990eb683e788c2fa7
edmund <edmundtay96@gmail.com>
2019-02-21 09:25:07 +0800

    Fix: the way mobile look

--- src/components/landing.js
+++ src/components/landing.js
@@ -9,16 +9,16 @@ const landing = () => {
   return (
     <section className="hero">
       <div className="container hero-body">
-        <div className="columns is-12 is-mobile" style={{paddingLeft:'0.75rem'}}>
+        <div className="columns is-12">
           <div className="column is-1-tablet is-2-mobile">
-            <div className="image is-64x64 is-centered'" >
+            <div className="image is-64x64 is-32x32-mobile is-centered'" >
               <Image
                 imgsrc={`profilepic.jpeg`}
                 style={{borderRadius: '50%'}}
               />
             </div>
           </div>
-          <div className="column is-6-desktop  is-9-tablet is-10-mobile">
+          <div className="column is-6-desktop  is-9-tablet is-12-mobile" >
             <div className="title">
               <Fade bottom>Hi! I'm Edmund</Fade>
             </div>
